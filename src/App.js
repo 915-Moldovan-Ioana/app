43ffba6bc6cb82a61599657b4c226f04f3621b42
915-Moldovan-Ioana <moldovai>
2022-04-18 23:57:43 +0300

    Exercise1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import {Button} from 'reactstrap'
 
-function App() {
+function App(props) {
+  const [ state, setState ] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={state} onChange={(event) => setState(event.target.value)} type='text'/>
+      <Button onClick = {() => setState('')}/>
+      <label>
+        {state}
+      </label>
     </div>
   );
 }
